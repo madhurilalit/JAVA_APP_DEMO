c963c0e608f2e70ce283f0041a276cb2c9740681
Madhuri, Lalit <lalit.madhuri@accenture.com>
2018-07-03 08:57:20 +0530

    DOcker commands updated

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,1 +1,8 @@
 #!/bin/sh
+
+echo "This is a Build docker image stage"
+dockerPwd=$1
+echo $dockerPwd
+sudo docker build -t madhurilalit/javademo:1.0 .
+sudo docker login --username madhurilalit --password $dockerPwd
+sudo docker push madhurilalit/javademo:1.0
---
+++ dockerrun.sh
@@ -0,0 +1,1 @@
+sudo docker run -v /home/ubuntu/tomcat/:/var/lib/tomcat7/webapps/myapp -d -p 8081:8080 madhurilalit/javademo:1.0
