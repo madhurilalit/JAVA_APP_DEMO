c8b212fde0c6948faf6b5a779070aaa210a25ef1
Madhuri, Lalit <lalit.madhuri@accenture.com>
2018-07-03 09:46:57 +0530

    code of .sh files updated

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "This is a Application Run "
+echo "This stage would build docker image and push to dockerhub "
 
 buildnum=$1
 sudo docker run -v /home/ubuntu/tomcat/:/var/lib/tomcat7/webapps/myapp -d -p 8081:8080 madhurilalit/javademo:$buildnum
