18c3a8e7845b00a99be59226241f7e5fb325b0a6
Madhuri, Lalit <lalit.madhuri@accenture.com>
2018-07-03 12:39:18 +0530

    docker run file code update

--- dockerrun.sh
+++ dockerrun.sh
@@ -2,4 +2,17 @@
 echo "This stage would build docker image and push to dockerhub"
 
 buildnum=$1
+dockerRunningContainerId=$(sudo docker ps | grep 8081 | sudo docker ps -q)
+echo dockerRunningContainerId =$dockerRunningContainerId
+
+sudo docker stop $dockerRunningContainerId
 sudo docker run -v /home/ubuntu/tomcat/:/var/lib/tomcat7/webapps/myapp -d -p 8081:8080 madhurilalit/javademo:$buildnum
+
+exitstatus=$?
+
+if [ $exitstatus -eq 0 ];then
+   echo " Container started successfully"
+else
+   echo "Container start has issue"
+   exit 1
+fi
